849fa31e0cd5b2c9ed655c352de7255912069b8a
Sorel Ulriel <simosorel35@gmail.com>
2022-11-27 09:57:55 -0800

    tasks of the project <0x10-variadic_functions>

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,9 @@
 #ifndef __VARIADIC_FUNCTIONS_H__
 #define __VARIADIC_FUNCTIONS_H__
 
+#include <stdlib.h>
+#include <stdarg.h>
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
